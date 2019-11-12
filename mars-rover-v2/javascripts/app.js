5305c237aabd599af1582ccbed598c1ac69dfa62
Nicolas Williamson <22387@columbushs.com>
2019-11-12 14:45:42 -0500

    push

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,12 +1,36 @@
 // Rover Object Goes Here
 // ======================
-
+let Rover ={
+  direction: 'N'
+}
 // ======================
 function turnLeft(rover){
+  if(rover.direction == 'N'){ 
+    rover.direction = 'W'
+  }
+  else if(rover.direction == 'W'){ 
+    rover.direction = 'S'
+  }
+  
+  else if(rover.direction == 'S'){
+    rover.direction = 'E'
+  }
+  else if(rover.direction == 'E'){ 
+    rover.direction = 'N'
+  }
+
+
+  
   console.log("turnLeft was called!");
-}
 
 function turnRight(rover){
+  if(rover.direction == 'N'){ 
+    rover.direction = 'E'
+  }
+  else if(rover.direction == 'E'){
+    rover.direction = 'S'
+  }
+  else if(rover.direction)
   console.log("turnRight was called!");
 }
 
